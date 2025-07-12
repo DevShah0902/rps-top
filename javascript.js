4d90ee8ab5ef3c5ffa88acb76c4bc77232a884a9
DevShah0902 <devshah0902@gmail.com>
2025-07-11 17:13:51 -0700

    Finally finished, added win/loss/tie message and score to the final message

--- javascript.js
+++ javascript.js
@@ -1,4 +1,24 @@
-function getComputerChoice(){
+function playGame(){
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (i = 0; i < 5; i++){
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    }
+    
+    let scores = "The score was: \n Computer: " + computerScore + " You: " + humanScore;
+
+    if (humanScore > computerScore){
+        return scores + "\n YOU WON!"
+    }
+    else if (humanScore < computerScore){
+        return scores + "\n You lost :("
+    }
+    else{
+        return scores + "\n It's a tie."
+    } 
+    function getComputerChoice(){
     let random = Math.random();
     if (random <= 0.333){
         return "rock";
@@ -10,58 +30,54 @@ function getComputerChoice(){
         return "scissors";
     }
     
-}
-
-function getHumanChoice(){
-    let choice = prompt("Rock, Paper, or Scissors?");
-    return choice
-}
-
-
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
-
-    let computerText = "Your opponent chose " + computerChoice + "! \n";
-    if (computerChoice == humanChoice){
-        return "You both chose " + computerChoice + ", it's a tie!";
     }
-    else if (computerChoice == "rock"){
-        if (humanChoice == "scissors"){
-            computerScore++
-            return computerText + "Rock beats scissors, you lose!"
-        }
-        else if (humanChoice == "paper"){
-            return computerText + "Paper beats rock, you win!"
-            humanScore++
-        }  
+
+    function getHumanChoice(){
+        let choice = prompt("Rock, Paper, or Scissors?");
+        return choice
     }
-    else if (computerChoice == "scissors"){
-        if (humanChoice == "paper"){
-            computerScore++
-            return computerText +"Scissors beats paper, you lose!"
+
+    function playRound(humanChoice, computerChoice){
+        humanChoice = humanChoice.toLowerCase();
+
+        let computerText = "Your opponent chose " + computerChoice + "! \n";
+        if (computerChoice == humanChoice){
+            return "You both chose " + computerChoice + ", it's a tie!";
         }
-        else if (humanChoice == "rock"){
-            humanScore++
-            return computerText +"Rock beats scissors, you win!"
+        else if (computerChoice == "rock"){
+            if (humanChoice == "scissors"){
+                computerScore++
+                return computerText + "Rock beats scissors, you lose!"
+            }
+            else if (humanChoice == "paper"){
+                return computerText + "Paper beats rock, you win!"
+                humanScore++
+            }  
         }
-        
-    }
-    else if (computerChoice == "paper"){
-        if (humanChoice == "rock"){
-            computerScore++
-            return computerText +"Paper beats rock, you lose!"
+        else if (computerChoice == "scissors"){
+            if (humanChoice == "paper"){
+                computerScore++
+                return computerText +"Scissors beats paper, you lose!"
+            }
+            else if (humanChoice == "rock"){
+                humanScore++
+                return computerText +"Rock beats scissors, you win!"
+            }
+            
         }
-        else if (humanChoice == "scissors"){
-            humanScore++
-            return computerText +"Scissors beats paper, you win!"
+        else if (computerChoice == "paper"){
+            if (humanChoice == "rock"){
+                computerScore++
+                return computerText +"Paper beats rock, you lose!"
+            }
+            else if (humanChoice == "scissors"){
+                humanScore++
+                return computerText +"Scissors beats paper, you win!"
+            }
+            
         }
         
-    }
-    
-    }
+        }
+}
 
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
-    console.log(computerScore, humanScore)
+console.log(playGame());
